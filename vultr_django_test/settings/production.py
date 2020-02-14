e2d80b1e528b28850a788011f66b37233509f967
willypower <parkhyongsok@naver.com>
2020-02-15 01:45:51 +0900

    trimming secret

--- vultr_django_test/settings/production.py
+++ vultr_django_test/settings/production.py
@@ -6,14 +6,17 @@ path = "/run/secrets/"
 
 file = open(path+"MYSQL_DATABASE","r")
 MYSQL_DATABASE = file.read()
+MYSQL_DATABASE = MYSQL_DATABASE.rstrip().lstrip()
 file.close()
 
 file = open(path+"MYSQL_USER","r")
 MYSQL_USER = file.read()
+MYSQL_USER = MYSQL_USER.rstrip().lstrip()
 file.close()
 
 file = open(path+"MYSQL_PASSWORD","r")
 MYSQL_PASSWORD = file.read()
+MYSQL_PASSWORD = MYSQL_PASSWORD.rstrip().lstrip()
 file.close()
 
 DATABASES = {
