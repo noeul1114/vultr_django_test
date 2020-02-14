51348c89b4b25c053341b4a882be705e7ad217cc
ezen <parkhyongsok@naver.com>
2020-02-14 15:57:29 +0900

    docker secret get update

--- testing/views.py
+++ testing/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+# Create your views here
+import docker
+
+client = docker.from_env()
 
 
 def index(request):
-    return HttpResponse("Django nginx Test Accomplished!!")
+    return HttpResponse("Django nginx Test Accomplished!! + secret = "+client.secrets.list())
