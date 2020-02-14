062cd5854cbec775a4f7dc49f0db2ebb54003d47
willypower <parkhyongsok@naver.com>
2020-02-15 01:31:39 +0900

    secret in unix file system update

--- vultr_django_test/settings/production.py
+++ vultr_django_test/settings/production.py
@@ -2,12 +2,26 @@ from .base import *
 
 DEBUG = False
 
+path = "/run/secrets/"
+
+file = open(path+"MYSQL_DATABASE","r")
+MYSQL_DATABASE = file.read()
+file.close()
+
+file = open(path+"MYSQL_USER","r")
+MYSQL_USER = file.read()
+file.close()
+
+file = open(path+"MYSQL_PASSWORD","r")
+MYSQL_PASSWORD = file.read()
+file.close()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ["MYSQL_DATABASE"],
-        'USER': os.environ["MYSQL_USER"],
-        'PASSWORD': os.environ["MYSQL_PASSWORD"],
+        'NAME': MYSQL_DATABASE,
+        'USER': MYSQL_USER,
+        'PASSWORD': MYSQL_PASSWORD,
         'HOST': 'mariadb',
         'PORT': '3306',
     }
