174cb377aebc179627779f26e13d44984b03d1fe
ezen <parkhyongsok@naver.com>
2020-02-19 17:35:14 +0900

    cors module deleted

--- vultr_django_test/settings/base.py
+++ vultr_django_test/settings/base.py
@@ -41,11 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,15 +52,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-
-CORS_ORIGIN_ALLOW_ALL = False
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000',
-]
-
-
 
 ROOT_URLCONF = 'vultr_django_test.urls'
 
